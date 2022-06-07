5f5f8607892bd7df515a3a6bb8a80062349968da
Natnael-Tilahun <natnaeltilahun97@gmail.com>
2022-06-07 11:24:02 -0700

    Solve task 4

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     index = 1
     sum = 0
-    for index in range(1,len(sys.argv)):
+    for index in range(1, len(sys.argv)):
         sum += int(sys.argv[index])
     print(sum)
